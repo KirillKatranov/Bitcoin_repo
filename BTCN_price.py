76a0865f6ca0efc524dbb2da6f6213a1422b2bfd
Кирилл Катранов <katranovrpk@gmail.com>
2025-06-20 10:58:29 +0300

    Create dockerfile

--- BTCN_price.py
+++ BTCN_price.py
@@ -1,17 +1,10 @@
 import time
-import matplotlib
 import requests
-import matplotlib.pyplot as plt
-import numpy as np
 
-matplotlib.use('TkAgg')
 URL_FOR_BTCN_LATEST_PRICE = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
 
 prices = []
 times = []
-
-plt.ion()  # Включаем интерактивный режим (обновление без нового окна)
-fig, ax = plt.subplots()
 
 
 while True:
@@ -19,14 +12,5 @@ while True:
     price = float(response["price"])
     
     prices.append(price)
-    times.append(len(prices))  # просто индекс вместо времени
-
-    ax.clear()  # очищаем старый график
-    ax.plot(times, prices, label="BTC/USDT", color="blue", marker='o')
-    ax.set_title("Текущая цена BTC")
-    ax.set_xlabel("Тики (секунды)")
-    ax.set_ylabel("Цена, $")
-    ax.grid(True)
-    ax.legend()
-
-    plt.pause(1)  # обновление графика каждые 1 сек
+    print(prices)
+    time.sleep(1)
