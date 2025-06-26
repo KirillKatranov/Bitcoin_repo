fc017ba253da7688fc93aa889b5b1aaea332b7ae
Кирилл Катранов <katranovrpk@gmail.com>
2025-06-26 11:40:45 +0300

    Поменял параметры запуска контейнера

--- src/BTCN_price.py
+++ src/BTCN_price.py
@@ -1,14 +1,13 @@
 import time
 import requests
 
-from queries.orm import insert_data
+from queries.orm import create_tables, insert_data
 
 URL_FOR_BTCN_LATEST_PRICE = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
 
 prices = []
 times = []
-
-
+#create_tables_if_no_exist()
 while True:
     response = requests.get(URL_FOR_BTCN_LATEST_PRICE).json()
     price = float(response["price"])
--- src/fast.py
+++ src/fast.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-
-
 app = FastAPI()
 
+from api import some_endpoints
